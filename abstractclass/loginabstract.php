7d6eb4a3ab553ede32171add8d15952eff759fca
Guille2610 <guillermo.mas.cedeno@gmail.com>
2026-04-25 22:47:11 -0500

    agregar funcionalidades de login en clase abstracta

---
+++ abstractclass/loginabstract.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+require_once 'Authenticator.php';
+
+// Limpiar mensaje anterior
+if(!isset($_SESSION['flash'])){
+    $_SESSION['flash'] = null;
+}
+
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+
+    $auth = new Authenticator();
+
+    try {
+        $msg = $auth->login($_POST['username'], $_POST['password']);
+
+        $_SESSION['flash'] = [
+            "mensaje" => $msg,
+            "tipo" => "success"
+        ];
+
+    } catch (Exception $e) {
+
+        $_SESSION['flash'] = [
+            "mensaje" => $e->getMessage(),
+            "tipo" => "error"
+        ];
+    }
+
+    // Redirige después de guardar mensaje
+    header("Location: index.php");
+    exit();
+}
+?>
